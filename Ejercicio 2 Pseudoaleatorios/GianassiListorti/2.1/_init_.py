4ff248faba5c436029110396e541eabe56829e5c
Hernán Listorti <hernanlistorti@gmail.com>
2020-05-13 19:28:17 -0300

    asd

--- Ejercicio 2 Pseudoaleatorios/GianassiListorti/2.1/_init_.py
+++ Ejercicio 2 Pseudoaleatorios/GianassiListorti/2.1/_init_.py
@@ -9,24 +9,33 @@ def clearScreen():
     else:
         _ = system('clear')
 
-def doNumpy(n, seed, rango):
+def muestraNumpy(n, seed, rango):
     np.random.seed(seed)
-    array = np.zeros(n)
+    array = np.zeros(n, dtype=int)
     for i in range(n):
         array[i] = np.random.randint(rango[0], rango[1])
     return array
 
 if __name__ == "__main__":
     clearScreen()
-    rango = [1, 120]
+    rango = [1, 50]
     print("BIENVENIDO A LA SIMULACIÓN DE NÚMEROS PSEUDOALEATORIOS.")
     seed = int(float(input("Ingrese una semilla: ")))
     print("A continuación generaremos números aleatorioos con esa semilla, comparando dos métodos con el generador de Numpy")
     #Numpy
     print("Comparando 3 muestras del generador de Numpy con la misma semilla")
-    print(doNumpy(5, seed, rango))
-    print(doNumpy(5, seed, rango))
-    print(doNumpy(5, seed, rango))
-    middleSquare = GeneradorMiddleSquare(seed)
-    print(middleSquare.muestra(100,(1000,350))
-
+    print(muestraNumpy(5, seed, rango))
+    print(muestraNumpy(5, seed, rango))
+    print(muestraNumpy(5, seed, rango))
+    #GCL
+    print("Comparando 3 muestras del generador de GCL con la misma semilla")
+    generadorGCL = GeneradorGCL(seed)
+    print(generadorGCL.muestra(5, rango))
+    print(generadorGCL.muestra(5, rango))
+    print(generadorGCL.muestra(5, rango))
+    #Middle-Square
+    print("Comparando 3 muestras del generador de medios del cuadrado con la misma semilla")
+    generadorMQ = GeneradorMiddleSquare(seed)
+    print(generadorMQ.muestra(5, rango))
+    print(generadorMQ.muestra(5, rango))
+    print(generadorMQ.muestra(5, rango))
--- Ejercicio 2 Pseudoaleatorios/GianassiListorti/2.1/generadores.py
+++ Ejercicio 2 Pseudoaleatorios/GianassiListorti/2.1/generadores.py
@@ -6,27 +6,33 @@ class GeneradorMiddleSquare:
     def __init__(self, seed):
         self.seed = seed
 
-    def gen(self):
-        if len(str(self.seed)) == 4:
-            square = num * num
+    def gen(self, interval):
+        lower, upper = interval[0], interval[1]
+        x = self.seed
+        if len(str(x)) > 4:
+            x = x[:4]
+        while len(str(x)) < 4:
+            x = int(str(x)+"1")
+        while True:
+            square = x * x
             if len(str(square)) < 8:
                 need = 8 - len(str(square))
                 addZero = ""
-                for i in range(0,need):
+                for j in range(0,need):
                     addZero = str(0) + addZero
                 square = addZero + str(square)  
             else: 
                 square = str(square)
-                result = int(square[2] + square[3] + square[4] + square[5])
-            yield result
+            x = int(square[2] + square[3] + square[4] + square[5])
+            yield x
     
     def muestra(self, n, interval):
-        lower, upper = interval[0], interval[1]
         sample = []
-        glibc = self.gen()
+        generadorsito = self.gen(interval)
         for i in range(n):
-            observation = (upper - lower) * (next(glibc) / (m - 1)) + lower
+            observation = next(generadorsito)
             sample.append(int(observation))
+
         return sample
 
                 
@@ -38,23 +44,23 @@ class GeneradorGCL:
     def __init__(self, seed):
         self.seed = seed
 
-    def gen(self, a,c,m):     
+    def gen(self, interval):
+        lower, upper = interval[0], interval[1]
+        # parameters as in GNU C Library
+        a = 1103515245 #Multiplicador
+        c = 12345 #Incremento
+        m = 2**32 #Modulo   
         xi = self.seed
         while True:
             xf = (a * xi + c) % m
             xi = xf
-            yield xf
+            yield int((upper - lower) * (xf / (m - 1)) + lower)
 
     def muestra(self, n, interval):
-        lower, upper = interval[0], interval[1]
         sample = []
-        # parameters as in GNU C Library
-        a = 1103515245 #Multiplicador
-        c = 12345 #Incremento
-        m = 2**32 #Modulo
-        glibc = self.gen(a,c,m)
+        generadorsito = self.gen(interval)
         for i in range(n):
-            observation = (upper - lower) * (next(glibc) / (m - 1)) + lower
+            observation = next(generadorsito)
             sample.append(int(observation))
 
         return sample
